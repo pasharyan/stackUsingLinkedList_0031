cd35e929592027db80cdcefd5ad42146bb4dd8a3
pasharyan <pasharyan50@gmail.com>
2026-05-14 15:19:25 +0700

    masukkan case 1

--- stackusinginkedlist_0031.cpp
+++ stackusinginkedlist_0031.cpp
@@ -90,3 +90,9 @@ int main()
         cin >> choice;
 
         switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
