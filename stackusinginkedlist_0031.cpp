5dd2c32f4c3570e0adbc246c7681c55d858c9caa
pasharyan <pasharyan50@gmail.com>
2026-05-14 15:14:14 +0700

    masukkan void

--- stackusinginkedlist_0031.cpp
+++ stackusinginkedlist_0031.cpp
@@ -35,3 +35,10 @@ public:
         cout << "Push value: " << value << endl;
         return value;
     }
+
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
